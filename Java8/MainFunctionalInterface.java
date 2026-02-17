f2e61cc4c242afca6d1ec70f079af2038692b1b7
Alok Raj <alokra7651@gmail.com>
2026-02-17 09:54:12 +0530

    functional interface

--- Java8/MainFunctionalInterface.java
+++ Java8/MainFunctionalInterface.java
@@ -3,13 +3,19 @@ package Java8;
 @FunctionalInterface
 interface Add
 {
-    int calculate(int n1,int n2);
+    void calculate();
 
 }
 public class MainFunctionalInterface
 {
     public static void main(String[] args)
     {
-
+        Add a1 = ()->{
+            int a=12;
+            int b=13;
+            System.out.println("Sum is: "+(a+b));
+        };
+        a1.calculate();
+//        System.out.println("Sum= "+a1.calculate());
     }
 }
