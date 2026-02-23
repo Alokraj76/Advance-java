7a96541f5095221c4de1378914b192cec4441c32
Alok Raj <alokra7651@gmail.com>
2026-02-23 13:20:10 +0530

    record and arr to List conversion

---
+++ java16/MainRecords.java
@@ -0,0 +1,25 @@
+package java16;
+
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.List;
+
+record Student(int sid,String name, String course)
+{
+
+};
+public class MainRecords
+{
+    public static void main(String[] args)
+    {
+//        Student s = new Student(1,"Student 1","MCA");
+//        System.out.println(s.sid());
+//        System.out.println(s.name());
+//        System.out.println(s.course());
+//        System.out.println(s);
+//        int data[] = {23,20,45,56};
+//        List<Integer> list = Arrays.asList(data);
+//        System.out.println();
+//        System.out.println(Arrays.toString(list.toArray()));
+    }
+}
