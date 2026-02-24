0673d0c4720f4f88213de9723101ad37fe5e6c3e
Alok Raj <alokra7651@gmail.com>
2026-02-24 10:03:31 +0530

    foreach method and stream uses

--- java16/MainRecords.java
+++ java16/MainRecords.java
@@ -1,6 +1,5 @@
 package java16;
-
-import java.sql.SQLOutput;
+import java.util.function.Consumer;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,9 +16,14 @@ public class MainRecords
 //        System.out.println(s.name());
 //        System.out.println(s.course());
 //        System.out.println(s);
-//        int data[] = {23,20,45,56};
-//        List<Integer> list = Arrays.asList(data);
-//        System.out.println();
-//        System.out.println(Arrays.toString(list.toArray()));
+        int data[] = {23,20,45,56};
+//        List<int[]> list = Arrays.asList(data);
+//        System.out.println(list.get(0)[1]); // 0 ka second value
+//        System.out.println(list.get(0)[2]); // 0 ka third value
+        List<Integer> list1 = Arrays.stream(data).boxed().toList(); // use of stream, boxed, toList.
+        System.out.println(list1);
+        list1.forEach((value)-> System.out.println(value)); // use of foreach
+
+
     }
 }
