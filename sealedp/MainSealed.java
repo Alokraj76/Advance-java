e8b2b8805fc1980f5155286d6365fc0ab2d4ba5b
Alok Raj <alokra7651@gmail.com>
2026-02-25 22:12:48 +0530

    seales methods input

--- sealedp/MainSealed.java
+++ sealedp/MainSealed.java
@@ -2,17 +2,32 @@ package sealedp;
 
 sealed class Shape permits Circle,Rectangle //sealed class allow multiple inheritance
 {
-
+    public void print()
+    {
+        System.out.println("Shape permits");
+    }
 }
 final class Circle extends Shape
 {
-
+    public void print()
+    {
+        System.out.println("Circle");
+    }
 }
 non-sealed class Rectangle extends Shape
 {
-
+    public void disply()
+    {
+        System.out.println("Rectangle");
+    }
 }
 public class MainSealed
 {
-
+    public static void main(String[] args)
+    {
+        Circle c = new Circle();
+        Rectangle r = new Rectangle();
+        c.print();
+        r.print();
+    }
 }
