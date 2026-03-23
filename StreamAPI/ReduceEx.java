bd48e6f3e08011c3b7c4a7dfad6fedef80913c03
Alok Raj <alokra7651@gmail.com>
2026-03-23 11:55:03 +0530

    reduce ex

---
+++ StreamAPI/ReduceEx.java
@@ -0,0 +1,12 @@
+package StreamAPI;
+import java.util.*;
+public class ReduceEx
+{
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(34,12,45,76,34,56,54,32);
+        int sum = list.stream().reduce(0,(a,b)->a+b);
+        System.out.println("Sum= "+sum);
+        double avg = sum/list.size();
+        System.out.println("Avg= "+avg);
+    }
+}
