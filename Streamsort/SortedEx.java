378738625cb539ba58b82af6282110de1e5754ef
Alok Raj <alokra7651@gmail.com>
2026-03-24 09:44:11 +0530

    sorted

--- Streamsort/SortedEx.java
+++ Streamsort/SortedEx.java
@@ -1,9 +1,13 @@
 package Streamsort;
-
+import java.util.*;
 public class SortedEx
 {
     public static void main(String[] args)
     {
+        List<Integer> list = Arrays.asList(23,12,1,45,23,76,26,91);
+        System.out.println(list);
+        List<Integer> sortedList = list.stream().sorted((a,b)->b-a).toList(); // sorted(Comparator.reverseOrder()).
+        System.out.println(sortedList);
 
     }
 }
